a2c0673c74a020f211aacbfb4f0d3757d568c1ac
Manasa G <manasa.g7890@hotmail.com>
2023-08-27 12:56:52 +0530

    fetched(image file upload)

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer')
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+let upload = multer({dest: "./public/data/uploads"})
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//name type size
 
+app.post('/api/fileanalyse',upload.single("upfile"),(req,res)=>{
+  const {originalname, mimetype, size} = req.file 
+ res.json({
+  name:originalname,
+  type:mimetype,
+  size:size
+ })
+})
 
 
 const port = process.env.PORT || 3000;
